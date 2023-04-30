2dcae4cfdd25d04e656c1649828034fef5e1b4e5
Sebastian <sebastianploch@protonmail.com>
2023-04-30 11:43:57 +0100

    Added .eleventy.js file + filters for ISO + Readable date

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+const moment = require('moment');
+ 
+moment.locale('en');
+ 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+ 
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+};
