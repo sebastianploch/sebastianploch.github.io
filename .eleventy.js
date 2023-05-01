cdb730fde3e7bc98c0cfc91a02ba8feab7a69343
Sebastian <sebastianploch@protonmail.com>
2023-05-01 19:12:53 +0100

    clean-up

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,30 @@
-const moment = require('moment');
- 
-moment.locale('en');
- 
+const moment = require("moment");
+
+moment.locale("en");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter('dateIso', date => {
+  eleventyConfig.addFilter("dateIso", (date) => {
     return moment(date).toISOString();
   });
- 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+
+  eleventyConfig.addFilter("dateReadable", (date) => {
+    return moment(date).utc().format("LL"); // E.g. May 31, 2019
   });
 
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     // Optional, default is "---"
-    excerpt_separator: "<!-- excerpt -->"
+    excerpt_separator: "<!-- excerpt -->",
   });
+
+  eleventyConfig.setUseGitIgnore(false);
+
+  return {
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
 };
--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,11 +1,3 @@
-import "@popperjs/core"
-
 // Bootstrap JS and CSS
 import "bootstrap";
 import "./../css/main.scss";
-
-// Bootstrap Popover
-import "./vendor/popover";
-
-// Bootstrap Tooltip
-import "./vendor/tooltip";
