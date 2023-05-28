5b8eee39710053e8454051ac75df8383e20d1420
Sebastian <sebastianploch@protonmail.com>
2023-05-28 13:01:11 +0100

    installed prism-atom-dark theme for code syntax highlight

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,4 +2,4 @@
 import "bootstrap";
 import "./../css/main.scss";
 import "./../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
-import "./../../node_modules/prismjs-tomorrow-theme/prism-tomorrow.css";
+import "./../css/prism/prism-atom-dark.css";
