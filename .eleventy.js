975b4fbe68c7007ebe2f3b89b8a0fa7d4b3f630c
Sebastian <sebastianploch@protonmail.com>
2023-05-28 17:07:19 +0100

    Added 'image' shortcode using EleventyImage plugin

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ moment.locale("en");
 
 module.exports = function (eleventyConfig) {
   // Plugins
+  const eleventyImagePlugin = require("@11ty/eleventy-img");
   const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
   const eleventySyntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 
@@ -31,6 +32,25 @@ module.exports = function (eleventyConfig) {
     excerpt: true,
     // Optional, default is "---"
     excerpt_separator: "<!-- excerpt -->",
+  });
+
+  // Shortcodes
+  eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+    let metadata = await eleventyImagePlugin(src, {
+      widths: [300, 600],
+      formats: ["avif", "jpeg"],
+      urlPath: "/img/",
+      outputDir: "./_site/img/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return eleventyImagePlugin.generateHTML(metadata, imageAttributes);
   });
 
   eleventyConfig.setUseGitIgnore(false);
