58db8609868b4a7d7a8a0f68eb0c90b610685a10
Sebastian <sebastianploch@protonmail.com>
2023-05-28 12:22:09 +0100

    Added EleventyImg plugin

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,20 @@ const moment = require("moment");
 moment.locale("en");
 
 module.exports = function (eleventyConfig) {
+  // Plugins
+  const eleventyImgPlugin = require("@11ty/eleventy-img");
+  module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyImgPlugin);
+  };
+
+  // Layouts
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
+  // Passthrough Copies
   eleventyConfig.addPassthroughCopy("CNAME");
 
+  // Filters
   eleventyConfig.addFilter("dateIso", (date) => {
     return moment(date).toISOString();
   });
@@ -16,6 +25,7 @@ module.exports = function (eleventyConfig) {
     return moment(date).utc().format("LL"); // E.g. May 31, 2019
   });
 
+  // Parsing
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     // Optional, default is "---"
